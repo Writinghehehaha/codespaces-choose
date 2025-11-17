7de45bfb35988367928bf076df75568bae5e96b8
Writinghehehaha <gflagg@cps.edu>
2025-11-17 14:21:57 +0000

    chef

--- script.js
+++ script.js
@@ -8,18 +8,4 @@ let lasagnaBtn = document. querySelector(".lasagna");
 
 sweetBtn.addEventListener("click", function () {
   console.log("Hi! LEFT BTN");
-  question.innerHTML = "up or down";
-  sweetBtn.style.display = "none";
-  savoryBtn.style.display = "none";
-  cakeBtn.style.display = "initial";
-  pieBtn.style.display = "initial";
-});
-
-savoryBtn.addEventListener("click", function () {
-  console.log("Hi! LEFT BTN");
-  question.innerHTML = "up or down";
-  sweetBtn.style.display = "none";
-  savoryBtn.style.display = "none";
-  steakBtn.style.display = "initial";
-  lasagnaBtn.style.display = "initial";
-});
+  });
