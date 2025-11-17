be02f033114b57fa3a7ca2994165f55e106ae187
Writinghehehaha <gflagg@cps.edu>
2025-11-17 02:30:26 +0000

    chef

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ let sweetBtn = document.querySelector(".sweet");
 let savoryBtn = document.querySelector(".savory");
 let cakeBtn = document.querySelector(".cake");
 let pieBtn = document.querySelector(".pie");
+let steakBtn = document.querySelector(".steak");
+let lasagnaBtn = document. querySelector(".lasagna");
 
 sweetBtn.addEventListener("click", function () {
-  console.log("Hi!");
-    question.innerHTML = "Alright, cake or pie?";
-    sweetBtn.style.display = "none";
-    savoryBtn.style.display = "none";
-    cakeBtn.style.display = "initial";
-    pieBtn.style.display = "initial";
+  console.log("Hi! LEFT BTN");
+  question.innerHTML = "up or down";
+  sweetBtn.style.display = "none";
+  savoryBtn.style.display = "none";
+  cakeBtn.style.display = "initial";
+  pieBtn.style.display = "initial";
+});
+
+savoryBtn.addEventListener("click", function () {
+  console.log("Hi! LEFT BTN");
+  question.innerHTML = "up or down";
+  sweetBtn.style.display = "none";
+  savoryBtn.style.display = "none";
+  steakBtn.style.display = "initial";
+  lasagnaBtn.style.display = "initial";
 });
